756ba0099b95fdbc5f5a19b158f7b877c8316cc9
Andylaa10 <andylaa100@gmail.com>
2022-03-13 15:09:25 +0100

    l

--- EventManagement/src/Main.java
+++ EventManagement/src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/view/LoginView.fxml"));
-        primaryStage.setTitle("My Movie Collection");
+        primaryStage.setTitle("Event Management");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
