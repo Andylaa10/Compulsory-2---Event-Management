6feeb8b646f022ddef49d1ae8d74def79b8a4d74
Andylaa10 <andylaa100@gmail.com>
2022-03-13 22:30:36 +0100

    comments

--- EventManagement/src/gui/controller/LoginController.java
+++ EventManagement/src/gui/controller/LoginController.java
@@ -28,7 +28,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 import java.util.stream.Stream;
+
+import static java.lang.Thread.sleep;
 
 public class LoginController implements Initializable {
 
@@ -56,25 +59,29 @@ public class LoginController implements Initializable {
     private ImageView img4;
     @FXML
     private ImageView img5;
+    @FXML
+    private ImageView img6;
 
     private Admin admin = new Admin();
     private Customer customer = new Customer();
     private EventCoordinator eventCoordinator = new EventCoordinator();
 
+    //https://www.youtube.com/watch?v=mgw1sAvhjy8&ab_channel=JavaGuides
+    public void saveLogin() {
 
-    public void saveLogin(ActionEvent actionEvent) {
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setBorder(loginPane);
-        setFade(img1);
-        setFade(img2);
-        setFade(img3);
-        setFade(img4);
-        setFade(img5);
+        delayLight();
+
     }
 
+    /**
+     * This helps you, if you cant remember or have problems with login
+     * @param actionEvent
+     */
     public void help(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Please contact the administration");
@@ -83,6 +90,10 @@ public class LoginController implements Initializable {
         alert.showAndWait();
     }
 
+    /**
+     * This method send you to another view, if you have an account
+     * @throws IOException
+     */
     public void LogIn() throws IOException {
         if (passwordField.getText().equals(admin.getPassword()) && usernameField.getText().equals(admin.getUsername())) {
             Stage switcher = (Stage) btnLogin.getScene().getWindow();
@@ -133,15 +144,45 @@ public class LoginController implements Initializable {
         timeline.play();
     }
 
+    /**
+     * Delay the code and makes a star effect
+     */
+    private void delayLight(){
+        try{
+            setFade(img1);
+            Thread.sleep(500);
+            setFade(img2);
+            Thread.sleep(500);
+            setFade(img3);
+            Thread.sleep(500);
+            setFade(img4);
+            Thread.sleep(500);
+            setFade(img5);
+            Thread.sleep(200);
+            setFade(img6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Makes the images fade, this creates a star effect
+     * @param img
+     */
     private void setFade(ImageView img){
         //fade
         FadeTransition fade = new FadeTransition();
         fade.setNode(img);
-        fade.setDuration(Duration.millis(2500));
+        fade.setDuration(Duration.millis(3500));
         fade.setCycleCount(TranslateTransition.INDEFINITE);
         fade.setInterpolator(Interpolator.LINEAR);
         fade.setFromValue(0.4);
         fade.setToValue(0);
         fade.play();
+    }
+
+    public void Quit(ActionEvent actionEvent) {
+        System.exit(0);
     }
 }
