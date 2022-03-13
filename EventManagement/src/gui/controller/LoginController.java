f804fbdbce5219258d94873a5a7a2c3d8231e352
Andylaa10 <andylaa100@gmail.com>
2022-03-13 17:22:36 +0100

    Light Animation

--- EventManagement/src/gui/controller/LoginController.java
+++ EventManagement/src/gui/controller/LoginController.java
@@ -3,6 +3,10 @@ package gui.controller;
 import be.Admin;
 import be.Customer;
 import be.EventCoordinator;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,11 +17,21 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 public class LoginController implements Initializable {
 
@@ -30,6 +44,12 @@ public class LoginController implements Initializable {
     @FXML
     private Button btnLogin;
 
+    @FXML
+    private AnchorPane loginPane;
+
+    @FXML
+    private ImageView logo;
+
     private Admin admin = new Admin();
     private Customer customer = new Customer();
     private EventCoordinator eventCoordinator = new EventCoordinator();
@@ -40,7 +60,7 @@ public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        setBorder(loginPane);
     }
 
     public void help(ActionEvent actionEvent) {
@@ -77,5 +97,27 @@ public class LoginController implements Initializable {
             alert.setContentText("You can also try again");
             alert.showAndWait();
         }
+    }
+
+    //Beautiful color animation around the anchor pane
+    private void setBorder(AnchorPane pane) {
+
+        Color[] colors = Stream.of("darkorange", "tomato", "deeppink", "blueviolet", "steelblue", "cornflowerblue", "lightseagreen", "#6fba82", "chartreuse", "crimson")
+                .map(Color::web)
+                .toArray(Color[]::new);
+
+        List<Border> list = new ArrayList<>();
+
+        int mills[] = {-200};
+        KeyFrame keyFrames[]  = Stream.iterate(0, i -> i+1)
+                .limit(100)
+                .map(i -> new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, new Stop[]{new Stop(0, colors[i%colors.length]), new Stop(1, colors[(i+1)%colors.length])}))
+                .map(lg -> new Border(new BorderStroke(lg, BorderStrokeStyle.SOLID, new CornerRadii(5), new BorderWidths(2))))
+                .map(b -> new KeyFrame(Duration.millis(mills[0]+=200), new KeyValue(pane.borderProperty(), b, Interpolator.EASE_IN)))
+                .toArray(KeyFrame[]::new);
+
+        Timeline timeline = new Timeline(keyFrames);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 }
