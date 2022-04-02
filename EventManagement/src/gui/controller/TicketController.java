1c1c9617c5c0a81e27f01e775c7d3f95f779911f
Ferniss <mathiasf2000@hotmail.com>
2022-04-02 18:27:06 +0200

    Ticketcontroller with initialize is up

--- EventManagement/src/gui/controller/TicketController.java
+++ EventManagement/src/gui/controller/TicketController.java
@@ -1,4 +1,18 @@
 package gui.controller;
 
-public class TicketController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TicketController implements Initializable {
+    @FXML
+    private Text ticketTitle;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
